23cddcccb6ea6e386b80363cb6c1a7da03a40714
Jed Mitchener <jed.mitchener@gmail.com>
2016-01-25 13:17:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+adele_goldberg = Person.new
+adele_goldberg.talk
+adele_goldberg.walk
+
+alan_kay = Person.new
